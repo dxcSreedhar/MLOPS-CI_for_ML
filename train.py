93c47fa964d67a287baf58559933e9cc9b31e44d
smarripati <sreedhar.devops@gmail.com>
2021-04-20 19:28:46 +0000

    first commit

---
+++ train.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# Set random seed
+seed = 42
+
+################################
+########## DATA PREP ###########
+################################
+
+# Load in the data
+df = pd.read_csv("wine_quality.csv")
+
+# Split into train and test sections
+y = df.pop("quality")
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
+
+#################################
+########## MODELLING ############
+#################################
+
+# Fit a model on the train section
+regr = RandomForestRegressor(max_depth=2, random_state=seed)
+regr.fit(X_train, y_train)
+
+# Report training set score
+train_score = regr.score(X_train, y_train) * 100
+# Report test set score
+test_score = regr.score(X_test, y_test) * 100
+
+# Write scores to a file
+with open("metrics.txt", 'w') as outfile:
+        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+
+
+##########################################
+##### PLOT FEATURE IMPORTANCE ############
+##########################################
+# Calculate feature importance in random forest
+"train.py" 85L, 2709C
+
